4351f1d479277b07a60c441ea75f4f86f88e9418
James O'Neal <jamesponeal@gmail.com>
2017-03-13 19:30:31 -0700

    adds additional tests and get_ranking method

--- spec/leapfrog_spec.rb
+++ spec/leapfrog_spec.rb
@@ -24,10 +24,22 @@ describe Leapfrog do
     expect(user.age).to eq(35)
   end
 
+  it "should return a Hash" do
+    expect(user.get_ranking("0.26532","C")).to be_kind_of(Hash)
+  end
+
   it "must get a propensity and ranking response" do
     expect(user.get_ranking("0.26532","C")).to eq({"propensity"=>"0.26532", "ranking"=>"C"})
   end
 
+  it "should return the correct propensity" do
+    response = user.get_ranking("0.26532","C")
+    expect(response["propensity"]).to eq("0.26532")
+  end
 
+  it "should return the correct ranking" do
+    response = user.get_ranking("0.26532","C")
+    expect(response["ranking"]).to eq("C")
+  end
 
 end
